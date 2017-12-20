d4ebef74e015b09483371a8148225a949fd5d434
VladimirFrank <vladimir.frankov@gmail.com>
2017-12-20 23:28:57 +0700

    Initial commit

---
+++ src/main/java/ru/frank/controller/MainController.java
@@ -0,0 +1,15 @@
+package ru.frank.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    // Use this token to access the HTTP API:
+    private String token = "458631815:AAFChJilHO8JIkske5O0kXntuCGP68XTi3s";
+    private String botUserName = "MegaUltraBot";
+
+    
+
+
+}
